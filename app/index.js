c33de7ad03f502fde98d4a88c3a6cc845aeb057c
David Ng <davidng@hk01.com>
2019-01-22 11:14:12 +0800

    Integrate koa-oas3

--- app/index.js
+++ app/index.js
@@ -1,10 +1,24 @@
 const Koa = require('koa');
+const mount = require('koa-mount');
+const serve = require('koa-static');
 const bodyParser = require('koa-bodyparser');
+const {oas} = require('koa-oas3');
 const routes = require('./routes');
 
+const swaggerStaticResource = new Koa();
+swaggerStaticResource.use(serve(`${__dirname}/swagger`, {index: 'index.yml'}));
 const app = new Koa();
 
 app.use(bodyParser());
+app.use(mount('/', swaggerStaticResource));
+app.use(oas({
+  file: `${__dirname}/swagger/index.yml`,
+  uiEndpoint: '/docs',
+  errorHandler: (err, ctx) => {
+    throw err;
+  }
+}));
+
 
 app.use(routes.routes());
 app.use(routes.allowedMethods());
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,13 +2,8 @@ const Router = require('koa-router');
 
 const router = new Router();
 
-router.get('/foo', ctx => ctx.body={
+router.get('/api/foo', ctx => ctx.body={
   id: 'foo'
 });
-
-router.get('/bar', ctx => ctx.body={
-  id: 'bar'
-});
-
 
 module.exports = router;
